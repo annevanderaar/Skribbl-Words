ea0b244168de07e269e9bfc6fcb874fd469b8d62
Anne van der Aar <annevanderaar@canvascompany.nl>
2023-12-21 14:50:00 +0100

    used axios and php instead of node

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $content = $data['content'];
+
+    $to = "annevanderaar@hotmail.com";
+    $subject = "New words for Skribbl";
+
+    mail($to, $subject, $content);
+
+    echo 'Mailed';
+}
--- main.js
+++ main.js
@@ -1,54 +1,39 @@
-let addToDoButton = document.querySelector('#addWordButton');
-let sendButton = document.querySelector('#sendButton');
-let inputField = document.getElementById('inputFieldWord');
-let wordsContainer = document.getElementById('wordsContainer');
+let addToDoButton = document.querySelector("#addWordButton");
+let sendButton = document.querySelector("#sendButton");
+let inputField = document.getElementById("inputFieldWord");
+let wordsContainer = document.getElementById("wordsContainer");
 let words = [];
 
 function AddWord() {
     words.push(inputField.value);
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
+    var paragraph = document.createElement("p");
+    paragraph.classList.add("paragraph-styling");
     paragraph.innerText = inputField.value;
     wordsContainer.appendChild(paragraph);
     inputField.value = "";
-};
+}
 
 function SendEmail() {
-    const emailBody = `<html><h2>header</h2><strong>${words}</strong><br></br><em>Italic</em></html>`
-
-    var nodemailer = require('nodemailer');
-
-    var transporter = nodemailer.createTransport({
-        service: 'hotmail',
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    });
-
-    var mailOptions = {
-        from: 'words@example.com',
-        to: 'annevanderaar@hotmail.com',
-        subject: 'New words for Skribbl',
-        text: emailBody
-    };
-
-    if (words.length > 0) {
-        transporter.sendMail(mailOptions, function (error) {
-            if (error) {
-                alert(error)
-                return
-            }
-            alert('Success')
-            this.words = [];
+    axios({
+        method: "post",
+        url: "https://everlastingwild.nl/mail.php",
+        data: {
+            content: words,
+        },
+    })
+        .then(() => {
+            alert("Success");
+        })
+        .catch((err) => {
+            alert(err);
+            console.log(err);
         });
-    }
-};
+}
 
-addToDoButton.addEventListener('click', function () {
+addToDoButton.addEventListener("click", function () {
     AddWord();
 });
 
-sendButton.addEventListener('click', function () {
+sendButton.addEventListener("click", function () {
     SendEmail();
 });
